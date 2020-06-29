eabcd9ed4c0914db6292cc8be63515a9bda2c6f1
jhnaldo <jhpjhp0223@gmail.com>
2020-06-29 19:23:45 +0900

    Support -eval-ir:state option to show the final state

--- src/main/scala/kr/ac/kaist/ires/Command.scala
+++ src/main/scala/kr/ac/kaist/ires/Command.scala
@@ -44,9 +44,7 @@ case object CmdParse extends CommandObj("parse", CmdBase >> Parse) {
 case object CmdLoad extends CommandObj("load", CmdParse >> Load)
 
 // eval
-case object CmdEval extends CommandObj("eval", CmdLoad >> IREval) {
-  override def display(st: ir.State): Unit = println(ir.beautify(st))
-}
+case object CmdEval extends CommandObj("eval", CmdLoad >> IREval)
 
 // repl
 case object CmdREPL extends CommandObj("repl", CmdLoad >> IRREPL)
--- src/main/scala/kr/ac/kaist/ires/phase/IREval.scala
+++ src/main/scala/kr/ac/kaist/ires/phase/IREval.scala
@@ -13,19 +13,26 @@ case object IREval extends PhaseObj[State, IREvalConfig, State] {
     initialSt: State,
     iresConfig: IRESConfig,
     config: IREvalConfig
-  ): State = (new Interp(config.debugir, config.timeout))(initialSt)
+  ): State = {
+    val st = (new Interp(config.debugir, config.timeout))(initialSt)
+    if (config.state) println(beautify(st))
+    st
+  }
 
   def defaultConfig: IREvalConfig = IREvalConfig()
   val options: List[PhaseOption[IREvalConfig]] = List(
     ("debug-ir", BoolOption(c => c.debugir = true),
       "print each step of IR."),
     ("timeout", NumOption((c, i) => c.timeout = if (i == 0) None else Some(i)),
-      "set timeout of interpreter(second), 0 for unlimited.")
+      "set timeout of interpreter(second), 0 for unlimited."),
+    ("state", BoolOption(c => c.state = true),
+      "show the final state after the evaluation."),
   )
 }
 
 // IREval phase config
 case class IREvalConfig(
     var debugir: Boolean = false,
-    var timeout: Option[Long] = Some(10)
+    var timeout: Option[Long] = Some(10),
+    var state: Boolean = false
 ) extends Config
