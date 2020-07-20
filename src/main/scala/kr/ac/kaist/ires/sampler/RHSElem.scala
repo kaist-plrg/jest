8674e4783e9e6d75ad334fe0b72a0972c3e50f7c
jhnaldo <jhpjhp0223@gmail.com>
2020-07-20 15:13:54 +0900

    Revised JSON format of RHSElem

--- src/main/scala/kr/ac/kaist/ires/sampler/RHSElem.scala
+++ src/main/scala/kr/ac/kaist/ires/sampler/RHSElem.scala
@@ -7,17 +7,7 @@ import spray.json._
 case class RHSElem(rhsName: String, k: Int)
 
 object RHSElem extends DefaultJsonProtocol {
-  implicit object RHSElemFormat extends RootJsonFormat[RHSElem] {
-    def write(r: RHSElem) = JsObject(
-      "rhsName" -> JsString(r.rhsName),
-      "k" -> JsNumber(r.k)
-    )
-    def read(value: JsValue) = value.asJsObject.getFields("rhsName", "k") match {
-      case Seq(JsString(rhsName), JsNumber(k)) =>
-        RHSElem(rhsName, k.toInt)
-      case _ => throw new DeserializationException("RhsElem read fail")
-    }
-  }
+  implicit val RHSElemFormat: JsonFormat[RHSElem] = jsonFormat2(RHSElem.apply)
   implicit def ordering[E <: RHSElem]: Ordering[E] = new Ordering[E] {
     override def compare(x: E, y: E): Int = x.toString.compareTo(y.toString)
   }
