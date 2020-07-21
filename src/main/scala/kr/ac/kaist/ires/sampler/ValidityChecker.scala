fdf051abb79f7c9ec183d12074e73175393d7116
jhnaldo <jhpjhp0223@gmail.com>
2020-07-21 11:04:27 +0900

    Fixed bug in ValidityChecker - refresh engine

--- src/main/scala/kr/ac/kaist/ires/sampler/ValidityChecker.scala
+++ src/main/scala/kr/ac/kaist/ires/sampler/ValidityChecker.scala
@@ -4,18 +4,22 @@ import kr.ac.kaist.ires.model.Script
 import javax.script._
 
 object ValidityChecker {
-  val manager = new ScriptEngineManager
-  val engine = manager.getEngineByName("Graal.js")
-  val MESSAGE = "IRES-EXPECTED-EXCEPTION"
-
-  def apply(script: String): Boolean = if (engine == null) {
-    Console.err.println("[WARNING] ValidityChecker always return true because Graal.js is not yet installed in this environment.")
-    true
-  } else try {
-    val revised = s"'use strict'; throw '$MESSAGE'; $script"
-    engine.eval(revised)
-    false
-  } catch {
-    case e: ScriptException => e.toString.endsWith(MESSAGE)
+  def apply(script: String): Boolean = {
+    val manager = new ScriptEngineManager
+    val engine = manager.getEngineByName("Graal.js")
+    val MESSAGE = "IRES-EXPECTED-EXCEPTION"
+    if (engine == null) {
+      Console.err.println("[WARNING] ValidityChecker always return true because Graal.js is not yet installed in this environment.")
+      true
+    } else try {
+      val revised = s"'use strict'; throw '$MESSAGE'; $script"
+      engine.eval(revised)
+      false
+    } catch {
+      case e: ScriptException =>
+        val pass = e.toString.endsWith(MESSAGE)
+        if (!pass) println(script)
+        pass
+    }
   }
 }
