9e1f10d78a52d4d277141c5610b5b075c79a5a84
DJ <f52985@gmail.com>
2020-07-23 21:33:46 +0900

    Make the mutator also mutates PrimaryExpression

--- src/main/scala/kr/ac/kaist/ires/mutator/SimpleExprReplacer.scala
+++ src/main/scala/kr/ac/kaist/ires/mutator/SimpleExprReplacer.scala
@@ -11,6 +11,19 @@ object SimpleExprReplacer extends Mutator with Walker {
     if (rand.nextBoolean) choose(exprList)
     else super.walk(expr)
   }
+  lazy val primExprList = {
+    NRSampler.visited = scala.collection.immutable.Set()
+    (
+      NRSampler.PrimaryExpression(false, false) ++
+      NRSampler.PrimaryExpression(false, true) ++
+      NRSampler.PrimaryExpression(true, false) ++
+      NRSampler.PrimaryExpression(true, true)
+    ).toList
+  }
+  override def walk(expr: PrimaryExpression): PrimaryExpression = {
+    if (rand.nextBoolean) choose(primExprList)
+    else super.walk(expr)
+  }
   lazy val statementList = NRSampler.origStatement.toList
   override def walk(stmt: Statement): Statement = {
     if (rand.nextBoolean) choose(statementList)
