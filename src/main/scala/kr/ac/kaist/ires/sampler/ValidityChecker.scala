a42fe78224ae97d6c6cb4bddaeaa5c1f5ca7350f
jhnaldo <jhpjhp0223@gmail.com>
2020-08-12 01:10:18 +0900

    Added useNodeJs in ValidityChecker

--- src/main/scala/kr/ac/kaist/ires/sampler/ValidityChecker.scala
+++ src/main/scala/kr/ac/kaist/ires/sampler/ValidityChecker.scala
@@ -1,13 +1,23 @@
 package kr.ac.kaist.ires.sampler
 
-import kr.ac.kaist.ires.model.Script
+import java.io.OutputStream
 import javax.script._
+import kr.ac.kaist.ires.LINE_SEP
+import kr.ac.kaist.ires.generator.Generator
+import kr.ac.kaist.ires.model.Script
+import kr.ac.kaist.ires.util.Useful._
+import sys.process._
+import java.util.concurrent.LinkedBlockingQueue
+import io.Source
 
 object ValidityChecker {
-  def apply(script: String, debug: Boolean = false): Boolean = {
+  val MESSAGE = "IRES-EXPECTED-EXCEPTION"
+  def apply(script: String, debug: Boolean = false): Boolean =
+    useGraalJs(script, debug)
+
+  def useGraalJs(script: String, debug: Boolean = false): Boolean = {
     val manager = new ScriptEngineManager
     val engine = manager.getEngineByName("Graal.js")
-    val MESSAGE = "IRES-EXPECTED-EXCEPTION"
     if (engine == null) {
       Console.err.println("[WARNING] ValidityChecker always return true because Graal.js is not yet installed in this environment.")
       true
@@ -20,6 +30,27 @@ object ValidityChecker {
         val pass = e.toString.endsWith(MESSAGE)
         if (debug && !pass) println(script)
         pass
+    }
+  }
+
+  def useNodeJs(script: String, debug: Boolean = false): Boolean = {
+    var pass = false
+    val logger = ProcessLogger(_ => {}, str => if (str == MESSAGE) pass = true)
+    val status = s"""node --trace-uncaught -e "'use strict'; throw '$MESSAGE'; $script"""" ! logger
+    pass
+  }
+
+  def test: Unit = {
+    println(s"Loading samples...")
+    val samples = Generator.getSample().map(_.toString)
+    println(s"Finished loading.")
+    val (gres, graaljs) = time(samples.map(s => (s, useGraalJs(s))))
+    println(s"GraalJs took $graaljs ms.")
+    val (nres, nodejs) = time(samples.map(s => (s, useNodeJs(s))))
+    println(s"Node.js took $nodejs ms.")
+
+    ((gres zip nres) zip samples).foreach {
+      case ((g, n), s) => if (g != n) println(s)
     }
   }
 }
