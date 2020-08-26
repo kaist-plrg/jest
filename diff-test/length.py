ed42c2a415d0c4820335b3952c6305bcdacd22c1
jhnaldo <jhpjhp0223@gmail.com>
2020-08-26 14:26:55 +0900

    Updated length.py

--- diff-test/length.py
+++ diff-test/length.py
@@ -12,16 +12,18 @@ def main():
     parser.set_defaults(beautify=False)
     args = parser.parse_args()
 
-
     # check directory
-    total_len, cnt = 0.0, 0.0
+    total_len, total_loc, cnt = 0.0, 0.0, 0.0
     for filepath in glob.glob(args.dir + "/*.js"):
         # get file length
         with open(filepath, "r") as script:
             script_content = script.read()
             beautified = jsbeautifier.beautify(script_content)
-            total_len += len(script_content if not args.beautify else beautified)
+            target = script_content if not args.beautify else beautified
+            total_len += len(target)
+            total_loc += target.count('\n')
             cnt += 1
-    
-    print(f"avg length in {args.dir} {total_len/cnt}")
+
+    # print result
+    print(f"avg length in {args.dir} length: {total_len/cnt} LOC: {total_loc/cnt}")
 main()
